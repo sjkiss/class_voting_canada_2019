000650c6af3eeeb8617f12171b8db96027486740
sjkiss <sjkiss@gmail.com>
2020-06-01 08:53:30 -0400

    Added religion variable for ces80

--- R_Scripts/1_master_file.R
+++ R_Scripts/1_master_file.R
@@ -39,7 +39,7 @@ table(ces80$region, ces80$region80)
 table(ces80$vote, ces80$vote80)
 ##We just need to turn the variables that end with 80 into regularly named variables.
 ces80 %>% 
-  select(male=male80, region=region80, quebec=quebec80, age=age80, language=language80, party_id=party_id80, vote=vote80, union, union_both, degree, employment, sector, income, occupation)->ces80
+  select(male=male80, region=region80, quebec=quebec80, age=age80, language=language80, party_id=party_id80, vote=vote80, union, union_both, degree, employment, sector, income, occupation, religion=religion)->ces80
 names(ces80)
 #####SPLITTING THE 04-11 FILE
 
